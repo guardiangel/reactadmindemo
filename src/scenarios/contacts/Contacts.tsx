df25b4abfd26121a1d46603907f485d10bedd290
guardiangel <felix.sgq@gmail.com>
2023-08-27 16:05:39 -0400

    Invoices and Contacts tables

--- src/scenarios/contacts/Contacts.tsx
+++ src/scenarios/contacts/Contacts.tsx
@@ -1,7 +1,112 @@
 import React from "react";
 
+import { Box, useTheme } from "@mui/material";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { colorTokens } from "../../theme";
+import { mockDataContacts } from "../../data/userData";
+import Header from "../../components/Header";
+
 const Contacts = () => {
-  return <div>contacts</div>;
+  const theme = useTheme();
+  const colors = colorTokens(theme.palette.mode);
+  const columns = [
+    {
+      field: "id",
+      headerName: "ID",
+      flex: 0.5,
+    },
+    {
+      field: "registrarId",
+      headerName: "Registrar ID",
+    },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column-cell",
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "nuber",
+      textAlign: "left",
+    },
+    {
+      field: "phone",
+      headerName: "Phone Number",
+      flex: 1,
+    },
+    {
+      field: "address",
+      headerName: "Address",
+      flex: 1,
+    },
+    {
+      field: "city",
+      headerName: "City",
+      flex: 1,
+    },
+    {
+      field: "zipCode",
+      headerName: "Zip Code",
+      flex: 1,
+    },
+  ];
+  return (
+    <Box m="20px">
+      <Header title="CONTACTS" subTitle="List of Contacts" />
+      <Box
+        m="40px 0 0 0 "
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          //defined in the above columns
+          "& .name-column-cell": {
+            color: colors.redAccent[200],
+          },
+
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: colors.blueAccent[700],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400], //400 or 900 works better
+          },
+          "& .MuiDataGrid-footerContainer": {
+            backgroundColor: colors.blueAccent[700],
+            borderTop: "none",
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${colors.grey[100]} !important`,
+          },
+        }}
+      >
+        <DataGrid
+          sx={{
+            fontSize: "16px",
+          }}
+          rows={mockDataContacts}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: { pageSize: 25, page: 0 },
+            },
+          }}
+          slots={{ toolbar: GridToolbar }}
+        />
+      </Box>
+    </Box>
+  );
 };
 
 export default Contacts;
--- src/scenarios/invoices/Invoices.tsx
+++ src/scenarios/invoices/Invoices.tsx
@@ -1,7 +1,117 @@
 import React from "react";
 
+import { Box, useTheme, Typography } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { colorTokens } from "../../theme";
+import { mockDataInvoices } from "../../data/userData";
+import Header from "../../components/Header";
+
+interface InvoiceRow {
+  row: InvoiceType;
+}
+
+interface InvoiceType {
+  id: number;
+  name: string;
+  email: string;
+  cost: string;
+  phone: string;
+  date: string;
+}
+
 const Invoices = () => {
-  return <div>Invoices</div>;
+  const theme = useTheme();
+  const colors = colorTokens(theme.palette.mode);
+  const columns = [
+    {
+      field: "id",
+      headerName: "ID",
+      flex: 0.5,
+    },
+
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column-cell",
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "cost",
+      headerName: "Cost",
+      flex: 1,
+      type: "string",
+      renderCell: (params: InvoiceRow) => (
+        <Typography color={colors.greenAccent[300]}>
+          ${params.row.cost}
+        </Typography>
+      ),
+    },
+    {
+      field: "phone",
+      headerName: "Phone Number",
+      flex: 1,
+    },
+    {
+      field: "date",
+      headerName: "Date",
+      flex: 1,
+    },
+  ];
+  return (
+    <Box m="20px">
+      <Header title="Invoices" subTitle="List of Invoices Balance" />
+      <Box
+        m="40px 0 0 0 "
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          //defined in the above columns
+          "& .name-column-cell": {
+            color: colors.redAccent[200],
+          },
+
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: colors.blueAccent[700],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400], //400 or 900 works better
+          },
+          "& .MuiDataGrid-footerContainer": {
+            backgroundColor: colors.blueAccent[700],
+            borderTop: "none",
+          },
+          "& .MuiCheckbox-root": {
+            color: `${colors.greenAccent[300]} !important`,
+          },
+        }}
+      >
+        <DataGrid
+          sx={{
+            fontSize: "16px",
+          }}
+          checkboxSelection
+          rows={mockDataInvoices}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: { pageSize: 25, page: 0 },
+            },
+          }}
+        />
+      </Box>
+    </Box>
+  );
 };
 
 export default Invoices;
